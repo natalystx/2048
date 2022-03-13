a7d70bdf1589bc98c80f6e50f2a5b90396c56d30
natalyst <59143229@g.cmru.ac.th>
2022-03-13 21:16:11 +0700

    game is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import './App.scss';
+import _ from 'lodash';
+
+interface TitleProps {
+  value: number;
+  index: number;
+  color?: string;
+}
+
+type Tiles = number[];
+
+const Tile: FC<TitleProps> = ({ value, index, color }) => {
+  return (
+    <span
+      className="tile"
+      style={{
+        gridArea: `tile-${index}`,
+        background: value ? color : '#93c0bb',
+      }}
+    >
+      {value > 0 && value}
+    </span>
+  );
+};
+
+const randomTile = (tile: Tiles): Tiles => {
+  const isFirstTime = tile.every((num) => num === 0);
+  if (isFirstTime) {
+    while (tile.filter((item) => item > 0).length < 2) {
+      const randomIndex = Math.floor(Math.random() * 15);
+      if (!tile[randomIndex]) {
+        tile[randomIndex] = 2;
+      }
+    }
+
+    return tile;
+  }
+
+  let randomIndex = Math.floor(Math.random() * 15);
+  let added = false;
+  while (!added) {
+    if (!tile[randomIndex]) {
+      tile[randomIndex] = 2;
+      added = true;
+    } else {
+      randomIndex = Math.floor(Math.random() * 15);
+    }
+  }
+
+  return tile;
+};
+
+const calculateVerticalTiles = (tiles: Tiles, down?: boolean): Tiles => {
+  for (let index = 0; index <= 3; index++) {
+    let currentColumnTiles = tiles.filter(
+      (val, tileIndex) => tileIndex % 4 === index
+    );
+
+    let valuedTile = currentColumnTiles.filter((val) => val);
+    valuedTile = valuedTile.map((val, index) => {
+      if (val === valuedTile[index + 1]) {
+        valuedTile[index + 1] = 0;
+        return val * 2;
+      }
+
+      return val;
+    });
+    valuedTile = valuedTile.filter((val) => val);
+
+    const fillerArrey: Tiles = new Array(4 - valuedTile.length).fill(0);
+
+    currentColumnTiles = !down
+      ? [...valuedTile, ...fillerArrey]
+      : [...fillerArrey, ...valuedTile];
+
+    let useIndex = 0;
+
+    tiles = tiles.map((val, mapIndex) => {
+      if (mapIndex % 4 === index) {
+        const item = currentColumnTiles[useIndex];
+        useIndex++;
+        return item;
+      } else {
+        return val;
+      }
+    });
+  }
+  return tiles;
+};
+
+const calculateHorizontalTiles = (tiles: Tiles, right?: boolean): Tiles => {
+  let maxCheckpointIndex = 0;
+  let minCheckpointIndex = 0;
+
+  for (let index = 0; index <= 3; index++) {
+    minCheckpointIndex = maxCheckpointIndex;
+    maxCheckpointIndex += 4;
+    let currentRowTiles: Tiles = tiles.slice(
+      minCheckpointIndex,
+      maxCheckpointIndex
+    );
+
+    let valuedTile = currentRowTiles.filter((val) => val);
+    valuedTile = valuedTile.map((val, index) => {
+      if (val === valuedTile[index + 1]) {
+        valuedTile[index + 1] = 0;
+        return val * 2;
+      }
+
+      return val;
+    });
+    valuedTile = valuedTile.filter((val) => val);
+
+    const fillerArrey: Tiles = new Array(4 - valuedTile.length).fill(0);
+
+    currentRowTiles = !right
+      ? [...valuedTile, ...fillerArrey]
+      : [...fillerArrey, ...valuedTile];
+
+    tiles = [
+      ...tiles.slice(0, minCheckpointIndex),
+      ...currentRowTiles,
+      ...tiles.slice(maxCheckpointIndex, tiles.length),
+    ];
+  }
+  return tiles;
+};
 
 function App() {
+  // [0, 0, 0, 0,
+  //  0, 0, 0, 0,
+  //  0, 0, 0, 0];
+  const [tileValues, setTileValues] = useState<Tiles>(
+    randomTile([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  );
+
+  const colors: any = {
+    2: '#1f5048',
+    4: '#2d725e',
+    8: '#d8fff9',
+    16: '#b2fff3',
+    32: '#1ece9f',
+    64: '#6dff91',
+    128: '#72ea8e',
+    256: '#ea8572',
+    512: '#ffc7be',
+    1024: '#ffe3de',
+    2048: '#ea7272',
+  };
+
+  const manipulateTiles = useCallback(
+    (key: string, tile: Tiles, setter: (val: Tiles) => void) => {
+      let combined: Tiles;
+
+      switch (key) {
+        case 'ArrowUp':
+          combined = calculateVerticalTiles(tile);
+          setter(randomTile(combined));
+          break;
+        case 'ArrowDown':
+          combined = calculateVerticalTiles(tile, true);
+          setter(randomTile(combined));
+          break;
+        case 'ArrowLeft':
+          combined = calculateHorizontalTiles(tile);
+          setter(randomTile(combined));
+          break;
+        case 'ArrowRight':
+          combined = calculateHorizontalTiles(tile, true);
+          setter(randomTile(combined));
+          break;
+      }
+    },
+    []
+  );
+
+  const addEvent = (e: KeyboardEvent) => {
+    manipulateTiles(e.key, tileValues, setTileValues);
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', addEvent);
+
+    return () => {
+      window.removeEventListener('keydown', addEvent);
+    };
+  }, [tileValues]);
+
+  const renderTile = (tiles: Tiles) =>
+    tiles.map((value, index) => (
+      <Tile value={value} index={index} key={index} color={`#2d${value}`} />
+    ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="game-box">{renderTile(tileValues)}</div>
+      </div>
     </div>
   );
 }
